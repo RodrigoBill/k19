c331bb1c38e305cc227fce5fba55343132ad6160
RodrigoBill <ramosrodrigo@gmail.com>
2013-11-08 15:22:00 -0200

    Add CSS e Models Curso e Produto

--- src/br/com/k19/model/Produto.java
+++ src/br/com/k19/model/Produto.java
@@ -4,20 +4,30 @@ public class Produto {
 
 	private String nome;
 	private double preco;
+	private boolean exibir;
 	
+	public boolean isExibir() {
+		return exibir;
+	}
+
+	public void setExibir(boolean exibir) {
+		this.exibir = exibir;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
-	
-	
-	
+
 }
